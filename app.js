288e77d06f176a0995c7562338c72f8e8362236f
Elshod <xusanovna2006@gmail.com>
2024-11-15 10:32:55 +0500

    adding last

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 let components = document.querySelectorAll("#aside div");
-let blockComps = document.querySelectorAll("#document .element");
 let block = document.getElementById("document");
+let panel = document.getElementById("panel");
+let activeElement = null;
+block.innerHTML = JSON.parse(localStorage.getItem("html")) || "";
 const createElement = (what) => document.createElement(what);
+const addClickListener = (element) => {
+    element.addEventListener("click", () => {
+      activeElement = element;
+      panel.classList.toggle("non-active");
+    });
+  }
+
+const removeClickListener = (element) => {
+  if (activeElement) {
+    block.removeChild(element);
+    activeElement = null;
+    panel.classList.add("non-active");
+    localStorage.setItem("html", JSON.stringify(block.innerHTML));
+  }
+};
+document.querySelectorAll("#document .element").forEach((element) => {
+  addClickListener(element);
+});
 
 components.forEach((item) => {
   item.addEventListener("click", () => {
-    console.log('adasd');
+    console.log("adasd");
     const newElement = createElement(item.getAttribute("data-value"));
     newElement.classList.add("element");
     block.appendChild(newElement);
 
     localStorage.setItem("html", JSON.stringify(block.innerHTML));
-    newElement.addEventListener("dblclick", () => {
-      block.removeChild(newElement);
-    });
+    removeClickListener(newElement);
+    addClickListener(newElement);
   });
+});
+document.addEventListener("keyup", (e) => {
+  if (e.key === "Delete" || e.key === "Backspace") {
+    removeClickListener(activeElement);
+  }
 });
