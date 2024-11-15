e5be53cb9f8d96cc45d4e97f1a2f4f099ef13d4f
Elshod <xusanovna2006@gmail.com>
2024-11-15 09:41:45 +0500

    added news

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 let components = document.querySelectorAll("#aside div");
 let blockComps = document.querySelectorAll("#document .element");
 let block = document.getElementById("document");
-let activeItem = null;
 const createElement = (what) => document.createElement(what);
 
 components.forEach((item) => {
   item.addEventListener("click", () => {
+    console.log('adasd');
     const newElement = createElement(item.getAttribute("data-value"));
     newElement.classList.add("element");
     block.appendChild(newElement);
 
     localStorage.setItem("html", JSON.stringify(block.innerHTML));
-    blockComps = document.querySelectorAll("#document .element");
-
-    blockComps.forEach((item) => {
-      item.addEventListener("click", () => {
-        item.classList.toggle("togle");
-        activeItem = item;
-      });
+    newElement.addEventListener("dblclick", () => {
+      block.removeChild(newElement);
     });
   });
 });
